084743fcecd5539f4899e4b395311cde0ced6d20
Fredrik Hansen <futurityab.github@isthesthit.nu>
2019-11-19 19:43:14 +0100

    Initial commit

---
+++ twitter_reader_config.js
@@ -0,0 +1,11 @@
+var twitter_config = module.exports = {
+twitter: {
+    consumer_key: 'MyTwitterAuthConsumerKey',
+    consumer_secret: 'MyTwitterAuthConsumerSecret',
+    access_token: 'MyTwitterAuthAccessToken',
+    access_token_secret: 'MyTwitterAuthAccessTokenSecret'
+  },
+topics: ['AWS', 'VPC', 'EC2', 'RDS', 'S3'],
+languages: ['en', 'es', 'de', 'fr', 'ar', 'pt'],
+kinesis_delivery: 'SocialMediaAnalyticsBlogPo-IngestionFirehoseStream-1115U2C9KXSFO'
+}
